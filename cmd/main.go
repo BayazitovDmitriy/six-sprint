3ead7bbfae89897612a7602260621d4bca8fbbbb
BayazitovDmitriy <di.li123ha@gmail.com>
2025-12-11 14:55:18 +0300

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	lg := log.New(log.Writer(), "serv:", log.Ldate|log.Ltime)
+	serv := server.CreateRouter(lg)
+	if err := serv.ServStart(); err != nil {
+		lg.Fatal("Server error:", err)
+	}
 }
